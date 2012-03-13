5b7597fe6453138b96e422455646be4cefd31610
enrique.molinari <enrique.molinari@gmail.com>
2012-03-13 00:21:38 +0000

    moving code to controller

--- tntbooks/src/ar/cpfw/tntbooks/model/TntBookStore.java
+++ tntbooks/src/ar/cpfw/tntbooks/model/TntBookStore.java
@@ -1,45 +1,15 @@
 package ar.cpfw.tntbooks.model;
 
-import java.util.List;
-
 import ar.cpfw.core.Validatable;
 import ar.cpfw.tntbooks.model.exceptions.BusinessException;
 
-// A bit of data oriented programming in this class, as this class 
-// is the link between the non OO world to the OO world
+// This class's primary purpose is to validate that a Book belongs to the Tnt Editorial
 public class TntBookStore implements Validatable<Book> {
 
 	private static final String TNTEDITORIAL_ISBN_PREFIX = "AR123";
-	private CustomerAgenda customerAgenda;
-	private BookCatalog bookCatalog;
 
 	public TntBookStore() {
 
-	}
-
-	public boolean isValidCustomer(String clientId) {
-		return customerAgenda.exists(clientId);
-	}
-
-	public Book bookByIsbn(String isbn) {
-		return bookCatalog.bookByIsbn(isbn);
-	}
-
-	public void checkout(TntCart cart, String customerId) {
-		customerAgenda.purchase(customerId, cart);
-	}
-
-	public List<Ticket> purchases(String customerId) {
-		return customerAgenda.purchases(customerId);
-	}
-
-	// Spring needs this ...
-	public void setCustomerAgenda(CustomerAgenda customerAgenda) {
-		this.customerAgenda = customerAgenda;
-	}
-
-	public void setBookCatalog(BookCatalog bookCatalog) {
-		this.bookCatalog = bookCatalog;
 	}
 
 	@Override
